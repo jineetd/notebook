9d289a520fb3756098ca9ed4b0ead90b1fc9cd41
jineetd <f20170168@goa.bits-pilani.ac.in>
2019-06-11 21:01:55 +0530

    1

--- plugin.rb
+++ plugin.rb
@@ -3,7 +3,7 @@
 enabled_site_setting :notebook_enabled
 
 after_initialize do
-	load File.expand_path('../app/controllers/notebook_controller.rb',_FILE_)
+	load File.expand_path('../app/controllers/notebook_controller.rb',__FILE__)
 
 	Discourse::Application.routes.append do	
 		#map notebook to notebook controller's index
