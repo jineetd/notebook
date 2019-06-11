cf1b45baeaa57324ea687b92c4ddea9edf3d9913
jineetd <f20170168@goa.bits-pilani.ac.in>
2019-06-11 20:41:17 +0530

    update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,3 +1,12 @@
 #name:notebook
 #version:0.1.0
-enabled_site_setting: notebook_enabled
+enabled_site_setting :notebook_enabled
+
+after_initialize do
+	load File.expand_path('../app/controllers/notebook_controller.rb',_FILE_)
+
+	Discourse::Application.routes.append do	
+		#map notebook to notebook controller's index
+		get '/notebook' => 'notebook#index'
+	end
+end
